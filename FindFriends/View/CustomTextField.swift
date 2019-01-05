6e579f94e739e456b987d2f842fb06d1c84b91f1
Vasu Chand <Vasu.chand@mobikwik.com>
2019-01-05 12:44:06 +0530

    missing file added

---
+++ FindFriends/View/CustomTextField.swift
@@ -0,0 +1,56 @@
+//
+//  CustomTextField.swift
+//  FindFriends
+//
+//  Created by vasu on 05/01/19.
+//  Copyright © 2019 Vasu. All rights reserved.
+//
+
+import Foundation
+
+
+import UIKit
+
+class CustomTextField: UITextField {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.cornerRadius = 5
+        tintColor = UIColor.white
+        addDoneButtonOnKeyboard()
+        // backgroundColor = UIColor.primaryLight
+        
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        let insetBounds = CGRect(x: bounds.origin.x + 8, y: bounds.origin.y, width: bounds.size.width - 16, height: bounds.size.height)
+        return insetBounds
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        let insetBounds = CGRect(x: bounds.origin.x + 8, y: bounds.origin.y, width: bounds.size.width - 16, height: bounds.size.height)
+        return insetBounds
+    }
+    
+    func addDoneButtonOnKeyboard()
+    {
+        let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+        doneToolbar.barStyle = .default
+        
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(self.doneButtonAction))
+        
+        let items = [flexSpace, done]
+        doneToolbar.items = items
+        doneToolbar.sizeToFit()
+        
+        self.inputAccessoryView = doneToolbar
+    }
+    
+    @objc func doneButtonAction()
+    {
+        self.resignFirstResponder()
+    }
+    
+}
